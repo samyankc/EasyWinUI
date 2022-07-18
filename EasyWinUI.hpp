e2c23b21218f12e79f643d91f30d2229aa945828
samyankc <sam.yan.kc@gmail.com>
2022-07-18 21:04:36 +0800

    .
    .

---
+++ EasyWinUI.hpp
@@ -0,0 +1,14 @@
+#include <Windows.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <thread>
+#include <string_view>
+
+
+int main()
+{
+
+    return 0;
+}
