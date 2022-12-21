2d00239b2acf1d68e95da309c79a7a679cfe211b
chun <sam.yan.kc@gmail.com>
2022-12-21 18:43:34 +0800

    .

--- ThreadPool.h
+++ ThreadPool.h
@@ -15,23 +15,23 @@ template<std::size_t PoolSize = 4, typename TaskType = std::function<void()>>
 struct ThreadPool
 {
     constexpr static auto ThreadCount = PoolSize;
+
     using TaskQueueType = std::queue<TaskType>;
 
     struct Worker
     {
         TaskType Task;
-        //std::thread       Thread;
         std::atomic<bool> Idle;
         std::atomic<bool> Spinning;
 
         Worker() : Idle{ true }, Spinning{ true }
         {
-            //Thread =
             std::thread( [this] {
                 while( Spinning )
                 {
                     Idle.wait( true, std::memory_order_acquire );
                     if( Task ) Task();
+                    Task = nullptr;
                     Idle.store( true, std::memory_order_release );
                 }
             } ).detach();
@@ -55,35 +55,40 @@ struct ThreadPool
             Spinning.store( false );
             Task = nullptr;
             if( Idle ) Activate();
-            // Thread.join();
         }
     };
 
     inline static auto Workers = std::vector<Worker>( ThreadCount );
 
+    inline static auto AssignTask( TaskType&& NewTask )
+    {
+        if( auto IdleWorker = std::ranges::find_if( Workers, &Worker::Available );  //
+            IdleWorker != Workers.end() )
+        {
+            IdleWorker->Task = std::move( NewTask );
+            IdleWorker->Activate();
+            return true;
+        }
+        return false;
+    }
+
     inline static auto TaskQueue = TaskQueueType{};
     inline static auto TaskQueueFilled = std::atomic<bool>{};
     inline static auto TaskQueueMutex = std::mutex{};
-    inline static auto TaskDistributor =
-        ( std::thread( [] {
-              while( true )
-              {
-                  TaskQueueFilled.wait( false, std::memory_order_acquire );
-
-                  if( auto IdleWorker = std::ranges::find_if( Workers, &Worker::Available );
-                      IdleWorker != Workers.end() )
-                  {
-                      IdleWorker->Task = std::move( TaskQueue.front() );
-                      IdleWorker->Activate();
-                      {
-                          std::scoped_lock Lock( TaskQueueMutex );
-                          TaskQueue.pop();
-                          if( TaskQueue.empty() ) TaskQueueFilled.store( false, std::memory_order_release );
-                      }
-                  }
-              }
-          } ).detach(),
-          0 );
+    inline static auto TaskDistributor = ( std::thread( [] {
+                                               while( true )
+                                               {
+                                                   TaskQueueFilled.wait( false, std::memory_order_acquire );
+                                                   if( AssignTask( std::move( TaskQueue.front() ) ) )
+                                                   {
+                                                       std::scoped_lock Lock( TaskQueueMutex );
+                                                       TaskQueue.pop();
+                                                       if( TaskQueue.empty() )
+                                                           TaskQueueFilled.store( false, std::memory_order_release );
+                                                   }
+                                               }
+                                           } ).detach(),
+                                           0 );
 
     static void WaitComplete()
     {
@@ -92,14 +97,19 @@ struct ThreadPool
             TaskQueueFilled.notify_one();
             std::this_thread::yield();
         }
-        while( ! std::ranges::all_of( Workers, &Worker::Available ) ) std::this_thread::yield();
+        while( ! std::ranges::all_of( Workers, &Worker::Available ) )
+        {
+            //std::ranges::for_each( Workers, &Worker::Activate );
+            std::this_thread::yield();
+        }
     }
-
-    static auto async( std::launch, TaskType&& NewTask ) { return AddTask( std::move( NewTask ) ); }
 
     static void AddTask( TaskType&& NewTask )
     {
         std::scoped_lock Lock( TaskQueueMutex );
+
+        // try to skip Task Queue if empty
+        if( TaskQueue.empty() && AssignTask( std::move( NewTask ) ) ) return;
 
         TaskQueue.push( std::move( NewTask ) );
         TaskQueueFilled.store( true, std::memory_order_release );
@@ -110,7 +120,7 @@ struct ThreadPool
     {
         while( ! IncomingTaskQueue.empty() )
         {
-            async( std::move( IncomingTaskQueue.front() ) );
+            AddTask( std::move( IncomingTaskQueue.front() ) );
             IncomingTaskQueue.pop();
         }
     }
