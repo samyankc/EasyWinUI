cb9fc7dc0be22941476671ad0721bd23c94efa9a
chun <sam.yan.kc@gmail.com>
2023-01-29 22:18:40 +0800

    .

--- UnitTest/TestAlwaysReturn.cpp
+++ UnitTest/TestAlwaysReturn.cpp
@@ -3,6 +3,7 @@
 
 using namespace EasyMeta;
 
+
 using FuncPtr1 = int ( * )( int, char, int* );
 using FuncPtr2 = double ( * )( int, long long, double* );
 
@@ -14,17 +15,17 @@ auto Consume( GetSizeFunction F )
 auto Consume_FuncPtr1( FuncPtr1 F ) { return F( {}, {}, {} ); }
 auto Consume_FuncPtr2( FuncPtr2 F ) { return F( {}, {}, {} ); }
 
-TEST_CASE( "Operator() return same value", "[AlwaysReturn<N>]" )
+BOOST_AUTO_TEST_CASE( TestOne )
 {
-    REQUIRE( AlwaysReturn<123>() == 122 );
-    REQUIRE( AlwaysReturn<0>() == 0 );
-    REQUIRE( AlwaysReturn<-1>() == -1 );
-    REQUIRE( AlwaysReturn<-999>() == -999 );
+    BOOST_TEST( AlwaysReturn<123>() == 122 );
+    BOOST_TEST( AlwaysReturn<0>() == 0 );
+    BOOST_TEST( AlwaysReturn<-1>() == -1 );
+    BOOST_TEST( AlwaysReturn<-999>() == -999 );
 }
 
-TEST_CASE( "Accepting Different Function Pointers", "[AlwaysReturn<N>]" )
+BOOST_AUTO_TEST_CASE( TestTwo )
 {
-    REQUIRE( Consume( AlwaysReturn<123> ) == 123 );
-    REQUIRE( Consume_FuncPtr1( AlwaysReturn<123> ) == 123 );
-    REQUIRE( Consume_FuncPtr2( AlwaysReturn<123> ) == 123 );
+    BOOST_TEST( Consume( AlwaysReturn<123> ) == 123 );
+    BOOST_TEST( Consume_FuncPtr1( AlwaysReturn<123> ) == 123 );
+    BOOST_TEST( Consume_FuncPtr2( AlwaysReturn<123> ) == 123 );
 }
